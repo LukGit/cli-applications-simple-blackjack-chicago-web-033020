a959ded9fb7fa68e56b1e5ab69569b82c04bc37e
Shau Yin Luk <ivan.luk@icloud.com>
2020-03-06 20:29:45 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -15,14 +15,15 @@ end
 
 def prompt_user
   # code #prompt_user here
-  puts 
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
 end
 
